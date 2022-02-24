6502c07c5749f3f07071b42e9bd745f6c9f5c97a
Raquel Rulli <raquelrulli7@gmail.com>
2022-02-24 16:13:36 -0700

    Updating key

--- poster.js
+++ poster.js
@@ -20,12 +20,12 @@ $('#term').focus(function(){
 
             $('#poster').html('<div class="alert"><strong>Loading...</strong></div>');
 
-            $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=" + film + "&callback=?", function(json) {
+             $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=b31d8ff85307c7c22681a7bcfd3cefe3&query=" + film + "&callback=?", function(json) {
                if (json != "Nothing found."){                 
 console.log(json);
                      $('#poster').html('<p>Your search found: <strong>' + json.results[0].title + '</strong></p><img src=\"http://image.tmdb.org/t/p/w500/' + json.results[0].poster_path + '\" class=\"img-responsive\" >');
                   } else {
-                     $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=goonies&callback=?", function(json) {
+                   $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=b31d8ff85307c7c22681a7bcfd3cefe3&query=goonies&callback=?", function(json) {
                        
                        console.log(json);
                         $('#poster').html('<div class="alert"><p>We\'re afraid nothing was found for that search.</p></div><p>Perhaps you were looking for The Goonies?</p><img id="thePoster" src="http://image.tmdb.org/t/p/w500/' + json[0].poster_path + ' class="img-responsive" />');
