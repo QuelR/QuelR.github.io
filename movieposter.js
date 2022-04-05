d54c49d77c0790b15cac832c2050ecbfb5b3a864
Raquel Rulli <raquelrulli7@gmail.com>
2022-04-05 11:03:14 -0600

    Working on movie poster

--- movieposter.js
+++ movieposter.js
@@ -1,4 +1,4 @@
-﻿//console.log($.getJSON("https://api.themoviedb.org/3/discover/movie?api_key=b31d8ff85307c7c22681a7bcfd3cefe3"));
+//console.log($.getJSON("https://api.themoviedb.org/3/discover/movie?api_key=b31d8ff85307c7c22681a7bcfd3cefe3"));
 
 
 $('#term').focus(function () {
