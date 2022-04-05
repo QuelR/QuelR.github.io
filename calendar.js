f41ba8c28d5b9ac0b3fb28f05ba8be4448c0e5db
Raquel Rulli <raquelrulli7@gmail.com>
2022-04-05 10:13:36 -0600

    Updating calendar feat

--- calendar.js
+++ calendar.js
@@ -1,191 +1,52 @@
-
-<script>
-    // Start
-    _("#calendar").innerHTML = calendar();
-     
-    // short queySelector
-function _(s) {
-    return document.querySelector(s);
-}
-
-//show info
-function showInfo(event) {
-  // link
-  var url = "https://codepen.io/nakome/pen/EWBMzm.css";
-  // get json
-  getjson(url, function (obj) {
-    for (var key in obj) {
-      // if has envent add class
-      if (_('[data-id="' + key + '"]')) {
-        _('[data-id="' + key + '"]').classList.add("event");
-      }
-      if (event === key) {
-        _("#calendar_data").classList.toggle("show_data");
-        // template info
-        var data =
-          '<a href="#" class="hideEvent" ' +
-          'onclick="return hideEvent();">&times;</a>' +
-"<h3>" +
-    obj[key].type +
-    "</h3>" +
-    "<dl>" +
-    "<dt><dfn>Title:</dfn></dt><dd>" +
-    obj[key].title +
-    "</dd>" +
-    "<dt><dfn>Hour:</dfn></dt><dd>" +
-    obj[key].time +
-    "</dd>" +
-    "<dt><dfn>Venue:</dfn></dt><dd>" +
-    obj[key].venue +
-    "</dd>" +
-    "<dt><dfn>Location:</dfn></dt><dd>" +
-    obj[key].location +
-    "</dd>" +
-    "<dt><dfn>Description:</dfn></dt><dd>" +
-    obj[key].desc +
-    "</dd>" +
-    '<dt><dfn>More Info:</dfn></dt><dd><a href="' +
-    obj[key].more +
-    '" title="More info">Here</a></dd>' +
-    "</dl>";
-
-return (_("#calendar_data").innerHTML = data);
-      }
-    }
-  });
-return false;
-}
-// toggle event show or hide
-function hideEvent() {
-    _("#calendar_data").classList.toggle("show_data");
-}
-
-// simple calendar
-function calendar() {
-    // show info on init
-    showInfo();
-
-    // vars
-    var day_of_week = new Array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"),
-        month_of_year = new Array(
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December"
-        ),
-        Calendar = new Date(),
-        year = Calendar.getYear(),
-        month = Calendar.getMonth(),
-        today = Calendar.getDate(),
-        weekday = Calendar.getDay(),
-        html = "";
-
-    // start in 1 and this month
-    Calendar.setDate(1);
-    Calendar.setMonth(month);
-
-    // template calendar
-    html = "<table>";
-    // head
-    html += "<thead>";
-    html +=
-        '<tr class="head_cal"><th colspan="7">' +
-        month_of_year[month] +
-        "</th></tr>";
-    html +=
-        '<tr class="subhead_cal"><th colspan="7">' +
-        Calendar.getFullYear() +
-        "</th></tr>";
-    html += '<tr class="week_cal">';
-    for (index = 0; index < 7; index++) {
-        if (weekday == index) {
-            html += '<th class="week_event">' + day_of_week[index] + "</th>";
+var currentMonth = document.querySelector(".current-month");
+var calendarDays = document.querySelector(".calendar-days");
+var today = new Date();
+var date = new Date();
+currentMonth.textContent = date.toLocaleDateString("en-US", { month: 'long', year: 'numeric' });
+today.setHours(0, 0, 0, 0);
+renderCalendar();
+function renderCalendar() {
+    const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+    const totalMonthDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    const startWeekDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+    calendarDays.innerHTML = "";
+    let totalCalendarDay = 6 * 7;
+    for (let i = 0; i < totalCalendarDay; i++) {
+        let day = i - startWeekDay;
+        if (i <= startWeekDay) {
+            // adding previous month days
+            calendarDays.innerHTML += `<div class='padding-day'>${prevLastDay - i}</div>`;
+        } else if (i <= startWeekDay + totalMonthDay) {
+            // adding this month days
+            date.setDate(day);
+            date.setHours(0, 0, 0, 0);
+            let dayClass = date.getTime() === today.getTime() ? 'current-day' : 'month-day';
+            calendarDays.innerHTML += `<div class='${dayClass}'>${day}</div>`;
         } else {
-            html += "<th>" + day_of_week[index] + "</th>";
+            // adding next month days
+            calendarDays.innerHTML += `<div class='padding-day'>${day - totalMonthDay}</div>`;
         }
     }
-    html += "</tr>";
-    html += "</thead>";
-
-    // body
-    html += '<tbody class="days_cal">';
-    html += "</tr>";
-
-    // white zone
-    for (index = 0; index < Calendar.getDay(); index++) {
-        html += '<td class="white_cal"> </td>';
-    }
-
-    for (index = 0; index < 31; index++) {
-        if (Calendar.getDate() > index) {
-            week_day = Calendar.getDay();
-
-            if (week_day === 0) {
-                html += "</tr>";
-            }
-            if (week_day !== 7) {
-                // this day
-                var day = Calendar.getDate();
-                var info =
-                    Calendar.getMonth() + 1 + "/" + day + "/" + Calendar.getFullYear();
-
-                if (today === Calendar.getDate()) {
-                    html +=
-                        '<td><a class="today_cal" href="#" data-id="' +
-                        info +
-                        '" onclick="return showInfo(\'' +
-                        info +
-                        "')\">" +
-                        day +
-                        "</a></td>";
-
-                    showInfo(info);
-                } else {
-                    html +=
-                        '<td><a href="#" data-id="' +
-                        info +
-                        '" onclick="return showInfo(\'' +
-                        info +
-                        "')\">" +
-                        day +
-                        "</a></td>";
-                }
-            }
-
-            if (week_day == 7) {
-                html += "</tr>";
-            }
-        }
-
-        Calendar.setDate(Calendar.getDate() + 1);
-    } // end for loop
-    return html;
 }
-
-//   Get Json data
-function getjson(url, callback) {
-    var self = this,
-        ajax = new XMLHttpRequest();
-    ajax.open("GET", url, true);
-    ajax.onreadystatechange = function () {
-        if (ajax.readyState == 4) {
-            if (ajax.status == 200) {
-                var data = JSON.parse(ajax.responseText);
-                return callback(data);
-            } else {
-                console.log(ajax.status);
-            }
-        }
-    };
-    ajax.send();
-}
- 
-</script >
+document.querySelectorAll(".soft-btn").forEach(function (element) {
+    element.addEventListener("click", function () {
+        date = new Date(currentMonth.textContent);
+        date.setMonth(date.getMonth() + (element.classList.contains("prev") ? -1 : 1));
+        currentMonth.textContent = date.toLocaleDateString("en-US", { month: 'long', year: 'numeric' });
+        renderCalendar();
+    });
+});
+document.querySelectorAll(".btn").forEach(function (element) {
+    element.addEventListener("click", function () {
+        let btnClass = element.classList;
+        date = new Date(currentMonth.textContent);
+        if (btnClass.contains("today"))
+            date = new Date();
+        else if (btnClass.contains("prev-year"))
+            date = new Date(date.getFullYear() - 1, 0, 1);
+        else
+            date = new Date(date.getFullYear() + 1, 0, 1);
+        currentMonth.textContent = date.toLocaleDateString("en-US", { month: 'long', year: 'numeric' });
+        renderCalendar();
+    });
+});
